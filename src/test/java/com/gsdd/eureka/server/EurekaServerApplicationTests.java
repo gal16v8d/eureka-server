9d46a669500fbd01a01d5ba1065462201f73786d
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-13 00:08:45 -0500

    Update project structure and add sonar props

--- src/test/java/com/gsdd/eureka/server/EurekaServerApplicationTests.java
+++ src/test/java/com/gsdd/eureka/server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package co.com.gsdd.eureka.server;
+package com.gsdd.eureka.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
